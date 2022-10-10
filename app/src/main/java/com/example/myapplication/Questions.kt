a3a4f1d72776f74f4b56dd67c3b2f4c7cd3ced32
20120230 <duytruong5437@gmail.com>
2022-10-10 23:25:40 +0700

    create fake quiz data

---
+++ app/src/main/java/com/example/myapplication/Questions.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+data class Questions(
+    val id: Int,
+    val questions: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int,
+
+)
