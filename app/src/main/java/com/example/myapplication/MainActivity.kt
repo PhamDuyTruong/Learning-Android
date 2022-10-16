f0371bc1b3f83d750de20661be9ed1176c174fe9
20120230 <duytruong5437@gmail.com>
2022-10-16 23:54:19 +0700

    create second ui continue

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+
 
         val btnStart: Button = findViewById(R.id.btn_start);
         val etName: EditText = findViewById(R.id.et_name);
--- app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
@@ -2,13 +2,48 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    private var progressBar : ProgressBar?= null
+    private  var tvProgress : TextView?= null;
+    private var tvQuestions: TextView?= null;
+    private var ivImage : ImageView?= null
+
+    private var tvOptionOne: TextView?= null;
+    private var tvOptionTwo: TextView?= null;
+    private var tvOptionThree: TextView?= null;
+    private var tvOptionFour: TextView?= null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+        progressBar = findViewById(R.id.progressBar);
+        tvProgress = findViewById(R.id.tv_progress);
+        tvQuestions = findViewById(R.id.tv_question);
+        ivImage = findViewById(R.id.iv_image);
+
+        tvOptionOne = findViewById(R.id.tv_option_one);
+        tvOptionTwo = findViewById(R.id.tv_option_two);
+        tvOptionThree = findViewById(R.id.tv_option_three);
+        tvOptionFour = findViewById(R.id.tv_option_four);
+
 
         val questionsList = Conststants.getQuestions();
+        val currentPosition = 1 // Default and the first question position
+        val question: Questions? =
+            questionsList[currentPosition - 1];
+        progressBar?.progress =
+            currentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text =
+            "$currentPosition" + "/" + progressBar?.getMax();
 
+        tvQuestions?.text = question!!.questions
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
 }
